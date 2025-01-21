83d09fb8ccdec0eb1c078e91168eb68aad7d70d4
Kevin Friesen <kfriesen@dp-otdsde-p037.traveltainment.int>
2025-01-21 11:34:39 +0100

    Add Themes

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -14,6 +14,10 @@ return {
     event = "BufRead",
     config = function() require("lsp_signature").setup() end,
   },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin", priority = 1000
+  },
 
   -- == Examples of Overriding Plugins ==
 
